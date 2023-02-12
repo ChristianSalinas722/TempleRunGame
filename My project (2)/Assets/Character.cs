c051bf579d3f2760e352e086796f8465b8e42128
Christian Salinas <50554683+ChristianSalinas722@users.noreply.github.com>
2023-02-12 01:16:08 -0600

    Update Character.cs

--- My project (2)/Assets/Character.cs
+++ My project (2)/Assets/Character.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public float speed = 5f;
+    public SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
